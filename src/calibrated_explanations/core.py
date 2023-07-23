f43e451499bf2e5c092519481128dfaca3e84d69
Tuwe Löfström <tuvelofstrom@users.noreply.github.com>
2023-07-24 00:06:25 +0200

    dev version bump

--- src/calibrated_explanations/core.py
+++ src/calibrated_explanations/core.py
@@ -25,7 +25,7 @@ from ._explanations import CalibratedExplanation
 from ._discretizers import BinaryDiscretizer, BinaryEntropyDiscretizer, DecileDiscretizer, QuartileDiscretizer, EntropyDiscretizer 
 from .VennAbers import VennAbers
 
-__version__ = 'v0.0.6'
+__version__ = 'v0.0.7'
 
 class CalibratedExplainer:
     """
