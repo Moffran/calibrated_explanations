082e1d95c6aa14a6b193a60b284584d6b87c1712
Tuwe Löfström <tuvelofstrom@users.noreply.github.com>
2023-08-08 21:49:16 +0200

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,13 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../notebooks'))
+sys.path.insert(0, os.path.abspath('../src/calibrated_explanations'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Calibrated-explanations'
+project = 'calibrated_explanations'
 copyright = '2023, Helena Löfström, Tuwe Löfström'
 author = 'Helena Löfström, Tuwe Löfström'
 
