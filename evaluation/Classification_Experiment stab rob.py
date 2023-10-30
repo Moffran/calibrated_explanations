f17f90c09994587b0d50fdda291b1db2e8eddca3
Tuwe Löfström <tuvelofstrom@users.noreply.github.com>
2023-10-30 16:34:35 +0100

    pylinting

--- evaluation/Classification_Experiment stab rob.py
+++ evaluation/Classification_Experiment stab rob.py
@@ -1,10 +1,8 @@
+# pylint: disable=invalid-name, line-too-long
+"""
+Experiment used in the introductory paper to evaluate the stability and robustness of the explanations
 """
 
-"""
-
-# from experiment_utils import (ece, clip)
-
-import copy
 import time
 import warnings
 import pickle
@@ -14,29 +12,18 @@ import xgboost as xgb
 from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-# from sklearn.calibration import CalibratedClassifierCV
-# from sklearn.metrics import (brier_score_loss, log_loss, mean_absolute_error, mean_squared_error)
 from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import StratifiedKFold
-# from sklearn.calibration import calibration_curve
-# from lime.lime_tabular import LimeTabularExplainer 
-# from shap import Explainer
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.calibration import CalibratedClassifierCV
 from venn_abers import VennAbers
 from calibrated_explanations import CalibratedExplainer
-# from lipschitz_metric import lipschitz_metric, get_perturbations, explainer_func_wrapper
-# from tqdm import tqdm
 
 
-
+# pylint: disable=invalid-name, missing-function-docstring, unused-argument
 def bin_total(y_true, y_prob, n_bins):
     bins = np.linspace(0., 1. + 1e-8, n_bins + 1)
-    # In sklearn.calibration.calibration_curve,
-    # the last value in the array is always 0.
     binids = np.digitize(y_prob, bins) - 1
     return np.bincount(binids, minlength=len(bins))
 
+# pylint: disable=missing-function-docstring
 def ece(y_true, y_prob, fop, mpv, n_bins=10):
     bins = bin_total(y_true, y_prob, n_bins)
     bins = bins[bins != 0]
@@ -53,7 +40,7 @@ def clip(val, min_=0, max_=1):
 def debug_print(message, debug=True):
     if debug:
         print(message)
-        
+
 # ------------------------------------------------------
 
 test_size = 20 # Antal foldar ändrade från 10
@@ -63,12 +50,12 @@ is_debug = True
 num_rep = 30
 
 descriptors = ['uncal','va',]#,'va'
-Descriptors = {'uncal':'Uncal','va': 'VA'} 
+Descriptors = {'uncal':'Uncal','va': 'VA'}
 models = ['xGB','RF'] # ['xGB','RF','DT','SVM',] # 'NN',
-explainers = ['ce','lime','shap'] #  ---------------------------->tagit bort  , sist
 
+# pylint: disable=line-too-long
 datasets = {1:"pc1req",2:"haberman",3:"hepati",4:"transfusion",5:"spect",6:"heartS",7:"heartH",8:"heartC",9:"je4243",10:"vote",11:"kc2",12:"wbc",
-            13:"kc3",14:"creditA",15:"diabetes",16:"iono",17:"liver",18:"je4042",19:"sonar", 20:"spectf",21:"german",22:"ttt",23:"colic",24:"pc4",25:"kc1"} # -------------------> tagit bort  , efter sista termen
+            13:"kc3",14:"creditA",15:"diabetes",16:"iono",17:"liver",18:"je4042",19:"sonar", 20:"spectf",21:"german",22:"ttt",23:"colic",24:"pc4",25:"kc1"}
 klara = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 tic_all = time.time()
 
@@ -99,7 +86,7 @@ for dataset in klara:
     h2 = HistGradientBoostingClassifier()
     g1 = xgb.XGBClassifier(objective='binary:logistic',use_label_encoder=False,eval_metric='logloss')
     g2 = xgb.XGBClassifier(objective='binary:logistic',use_label_encoder=False,eval_metric='logloss')
-    
+
     model_dict = {'xGB':(g1,g2,"xGB",Xn),'RF':(r1,r2,"RF",Xn),'SVM': (s1,s2,"SVM",Xn),'DT': (t1,t2,"DT",Xn),'HGB': (h1,h2,"HGB",Xn)}#,'NN': (a1,a2,"NN",Xn)
     model_struct = [model_dict[model] for model in models]
     results[dataSet] = {}
@@ -108,17 +95,17 @@ for dataset in klara:
         debug_print(dataSet+' '+alg)
         results[dataSet][alg] = {}
 
-        calibrators = {} 
+        calibrators = {}
         for desc in descriptors:
             calibrators[desc] = {}
             calibrators[desc]['ce'] = []
         trainCalX, testX, trainCalY, testY = train_test_split(X.values, y.values, test_size=test_size,random_state=42)
         trainX, calX, trainY, calY = train_test_split(trainCalX, trainCalY, test_size=0.33,random_state=42)
-        
+
         c2.fit(trainX,trainY)
 
         calibrators['uncal']['model'] = c2
-        if descriptors.__contains__('va'):                        
+        if 'va' in descriptors:
             calibrators['va']['model'] = VennAbers()
             calibrators['va']['model'].fit(c2.predict_proba(calX), calY)
         calibrators['data'] = {'trainX':trainX,'trainY':trainY,'calX':calX,'calY':calY,'testX':testX,'testY':testY,}
@@ -126,19 +113,19 @@ for dataset in klara:
         # debug_print(dataSet + ' ' + alg , is_debug)
         np.random.seed(1337)
         categorical_features = [i for i in range(no_of_features) if len(np.unique(X.iloc[:,i])) < 10]
-        
+
         ce = CalibratedExplainer(c2, calX, calY, \
             feature_names=df.columns, categorical_features=categorical_features)
 
-        stability =  {'ce':[], 'cce':[], }#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]} 
+        stability =  {'ce':[], 'cce':[], }#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]}
         stab_timer = {'ce':[], 'cce':[], }#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]}
-        robustness = {'ce':[], 'cce':[], 'proba':[]}#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]} 
+        robustness = {'ce':[], 'cce':[], 'proba':[]}#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]}
         rob_timer =  {'ce':[], 'cce':[], }#'lime':[], 'lime_va':[], 'shap':[], 'shap_va':[]}
         i = 0
         while i < num_rep:
             try:
                 # print(f'{i}:',end='\t')
-                
+
                 ce.set_random_state(i)
                 tic = time.time()
                 factual_explanations = ce.explain_factual(testX)
@@ -146,7 +133,7 @@ for dataset in klara:
                 stab_timer['ce'].append(ct)
                 # print(f'{ct:.1f}',end='\t')
                 stability['ce'].append([f.feature_weights for f in factual_explanations])
-                
+
                 ce.set_random_state(i)
                 tic = time.time()
                 factual_explanation = ce.explain_counterfactual(testX)
@@ -155,14 +142,13 @@ for dataset in klara:
                 # print(f'{ct:.1f}',end='\t')
                 stability['cce'].append([f.feature_weights for f in factual_explanations])
                 i += 1
-            except Exception as e:
+            except Exception as e: # pylint: disable=broad-exception-caught
                 warnings.warn(f'Error: {e}')
             # print('')
-            
-        
+
         results[dataSet][alg]['stability'] = stability
         results[dataSet][alg]['stab_timer'] = stab_timer
-        
+
         i = 0
         while i < num_rep:
             np.random.seed(i)
@@ -171,7 +157,7 @@ for dataset in klara:
             else:
                 c2 = RandomForestClassifier(n_estimators=100, random_state=i)
             trainX, calX, trainY, calY = train_test_split(trainCalX, trainCalY, test_size=0.33,random_state=i)
-            
+
             c2.fit(trainX,trainY)
             ce = CalibratedExplainer(c2, calX, calY, \
                 feature_names=df.columns, categorical_features=categorical_features)
@@ -179,7 +165,6 @@ for dataset in klara:
 
             try:
                 # print(f'{i}:',end='\t')
-                
                 ce.set_random_state(i)
                 tic = time.time()
                 factual_explanations = ce.explain_factual(testX)
@@ -187,7 +172,7 @@ for dataset in klara:
                 rob_timer['ce'].append(ct)
                 # print(f'{ct:.1f}',end='\t')
                 robustness['ce'].append([f.feature_weights for f in factual_explanations])
-                
+
                 ce.set_random_state(i)
                 tic = time.time()
                 factual_explanation = ce.explain_counterfactual(testX)
@@ -196,17 +181,16 @@ for dataset in klara:
                 # print(f'{ct:.1f}',end='\t')
                 robustness['cce'].append([f.feature_weights for f in factual_explanations])
                 i += 1
-            except Exception as e:
+            except Exception as e: # pylint: disable=broad-exception-caught
                 warnings.warn(f'Error: {e}')
             # print('')
 
         results[dataSet][alg]['robustness'] = robustness
         results[dataSet][alg]['rob_timer'] = rob_timer
-    
 
-    toc_data = time.time()  
-    debug_print(dataSet + ': ' +str(toc_data-tic_data),is_debug )  
+    toc_data = time.time()
+    debug_print(dataSet + ': ' +str(toc_data-tic_data),is_debug )
     pickle.dump(results, open('evaluation/results_stab_rob.pkl', 'wb'))
 
-toc_all = time.time()    
-debug_print(str(toc_data-tic_data),is_debug ) 
+toc_all = time.time()
+debug_print(str(toc_data-tic_data),is_debug )
