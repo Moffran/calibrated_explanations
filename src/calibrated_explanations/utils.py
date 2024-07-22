04330cb3d9711d7dd51160221786a44e7a84341f
Tuwe Löfström <tuwe.lofstrom@ju.se>
2024-07-22 16:28:24 +0200

    fix: trailing whitespace

--- src/calibrated_explanations/utils.py
+++ src/calibrated_explanations/utils.py
@@ -151,7 +151,7 @@ def check_is_fitted(estimator, attributes=None, *, msg=None, all_or_any=all):
         fitted = [
             v for v in vars(estimator) if v.endswith("_") and not v.startswith("__")
         ]
-    
+
     if not fitted or fitted == []:
         raise RuntimeError(msg % {"name": type(estimator).__name__})
 
