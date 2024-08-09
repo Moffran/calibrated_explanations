856835f5d833602689a7813c656b2eef8bfd9a2a
Tuwe Löfström <tuwe.lofstrom@ju.se>
2024-08-10 01:09:41 +0200

    test: reinstated inactivated tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -72,7 +72,7 @@ class TestCalibratedExplainer_regression(unittest.TestCase):
             cal_exp.set_difficulty_estimator(DifficultyEstimator)
 
 
-    @unittest.skip('Skipping provisionally.')
+    # @unittest.skip('Skipping provisionally.')
     def test_regression_ce(self):
         X_prop_train, y_prop_train, X_cal, y_cal, X_test, _, _, _, categorical_features, categorical_labels, feature_names = load_regression_dataset()
         model, _ = get_regression_model('RF', X_prop_train, y_prop_train) # pylint: disable=redefined-outer-name
@@ -220,7 +220,7 @@ class TestCalibratedExplainer_regression(unittest.TestCase):
         self.assertExplanation(counter)
 
 
-    @unittest.skip('Skipping provisionally.')
+    # @unittest.skip('Skipping provisionally.')
     def test_regression_conditional_ce(self):
         X_prop_train, y_prop_train, X_cal, y_cal, X_test, _, _, _, categorical_features, categorical_labels, feature_names = load_regression_dataset()
         model, _ = get_regression_model('RF', X_prop_train, y_prop_train) # pylint: disable=redefined-outer-name
@@ -293,7 +293,7 @@ class TestCalibratedExplainer_regression(unittest.TestCase):
             pytest.fail(f"counterfactual_explanation.plot() raised unexpected exception: {e}")
 
 
-    @unittest.skip('Skipping provisionally.')
+    # @unittest.skip('Skipping provisionally.')
     def test_probabilistic_regression_conditional_ce(self):
         X_prop_train, y_prop_train, X_cal, y_cal, X_test, y_test, _, _, categorical_features, categorical_labels, feature_names = load_regression_dataset()
         model, _ = get_regression_model('RF', X_prop_train, y_prop_train) # pylint: disable=redefined-outer-name
@@ -351,7 +351,7 @@ class TestCalibratedExplainer_regression(unittest.TestCase):
             pytest.fail(f"counterfactual_explanation.plot() raised unexpected exception: {e}")
 
 
-    @unittest.skip('Skipping provisionally.')
+    # @unittest.skip('Skipping provisionally.')
     def test_knn_normalized_regression_ce(self):
         X_prop_train, y_prop_train, X_cal, y_cal, X_test, _, _, _, categorical_features, categorical_labels, feature_names = load_regression_dataset()
         model, _ = get_regression_model('RF', X_prop_train, y_prop_train) # pylint: disable=redefined-outer-name
@@ -482,7 +482,7 @@ class TestCalibratedExplainer_regression(unittest.TestCase):
             pytest.fail(f"counterfactual_explanation.plot() raised unexpected exception: {e}")
 
 
-    @unittest.skip('Skipping provisionally.')
+    # @unittest.skip('Skipping provisionally.')
     def test_var_normalized_regression_ce(self):
         X_prop_train, y_prop_train, X_cal, y_cal, X_test, _, _, _, categorical_features, categorical_labels, feature_names = load_regression_dataset()
         model, _ = get_regression_model('RF', X_prop_train, y_prop_train) # pylint: disable=redefined-outer-name
