b942267e25975a31e04522f37836c70258451f70
Tuwe Löfström <tuwe.lofstrom@ju.se>
2024-12-06 13:41:45 +0100

    feat: add module for explanations of calibrated models

---
+++ src/calibrated_explanations/explanations/__init__.py
@@ -0,0 +1,4 @@
+"""Module for explanations of calibrated models."""
+from .explanations import CalibratedExplanations, AlternativeExplanations
+from .explanation import CalibratedExplanation, FactualExplanation, \
+                    AlternativeExplanation, FastExplanation
